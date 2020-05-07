71e06c62caa4ecc6b76327fd3a17bb5bcf741104
Rene Avila <reneavila1993@gmail.com>
2020-05-07 18:19:34 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -48,3 +48,11 @@ def loop_message_n_times(message, n)
   counter += 1 
   end 
 end 
+
+def output_array(array)
+  index = 0 
+  while index < array.length do
+    puts array[index]
+    index += 1 
+  end
+end
